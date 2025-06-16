ace9f683ea11da354220bccdfffc3bb022961d77
singh-shreya-2007 <singhmanshi8916@gmail.com>
2025-06-16 15:21:52 +0530

    Add files via upload

---
+++ resize.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+from io import BytesIO
+
+st.header("Welcome to Image Resizer")
+st.title("Photo Resizer App Using OpenCV")
+
+img = st.file_uploader("Upload an image", type=["jpeg", "png"])
+
+if img:
+    img = Image.open(img)
+    array_image = np.array(img)
+    st.image(array_image, caption="Original Image", width=100)
+
+    width = st.number_input("Enter width", min_value=1, value=array_image.shape[1])
+    height = st.number_input("Enter height", min_value=1, value=array_image.shape[0])
+
+    flip_value = st.selectbox("Direction", ["None", "Vertical", "Horizontal"])
+    if flip_value == "Vertical":
+        array_image = cv2.flip(array_image, 0)
+    elif flip_value == "Horizontal":
+        array_image = cv2.flip(array_image, 1)
+
+    st.image(array_image, caption="Flipped Image", width=100)
+
+    if st.button("Resize"):
+        resized_image = cv2.resize(array_image, (int(width), int(height)))
+        st.image(resized_image, caption="Resized Image", width=100)
+
+        buff = BytesIO()
+        image_pil = Image.fromarray(resized_image)
+        image_pil.save(buff, format="JPEG")
+
+        st.download_button(
+            "Download Here",
+            data=buff.getvalue(),
+            mime="image/jpeg",
+            file_name="resized_image.jpeg"
+        )
